333aa12fd8f29951bb0a8df57bb31ba76f7eda01
Christian Wekode <christian@wekode.es>
2022-03-14 00:41:55 +0100

    remove udpate status from user

--- src/app/layout/common/user/user.component.ts
+++ src/app/layout/common/user/user.component.ts
@@ -65,30 +65,6 @@ export class UserComponent implements OnInit, OnDestroy
         this._unsubscribeAll.complete();
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Update the user status
-     *
-     * @param status
-     */
-    updateUserStatus(status: string): void
-    {
-        // Return if user is not available
-        if ( !this.user )
-        {
-            return;
-        }
-
-        // Update the user
-        this._userService.update({
-            ...this.user,
-            status
-        }).subscribe();
-    }
-
     /**
      * Sign out
      */
